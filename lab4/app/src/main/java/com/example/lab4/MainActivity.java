ddc195838a927bfa897070c2cfd47730ec5dec17
dinhtronghao <dinhtronghao12102002@gmail.com>
2023-07-25 21:22:55 +0700

    bt_lab4

---
+++ lab4/app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_frame, new Fragment_login())
+                    .commit();
+        }
+    }
+
+}
